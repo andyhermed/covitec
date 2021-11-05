1966b00d3198221b4ed5b864e0fa2dc5edcfe3ff
AnddyHub <andreabt40@gmail.com>
2021-11-04 20:30:25 -0600

    nuevo

--- usuarios.php
+++ usuarios.php
@@ -28,7 +28,7 @@ $resultado = $conn->query($sql);
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Sistema de Control </title>
+        <title>Sistema de Control de Acceso </title>
         <link rel="stylesheet" href="css/style.css">
     </head>
 
