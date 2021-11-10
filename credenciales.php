fde0b66181954c58337e01afa18f956eb8aedc2c
andyhermed <93298418+andyhermed@users.noreply.github.com>
2021-11-09 22:54:38 -0600

    Update credenciales.php

--- credenciales.php
+++ credenciales.php
@@ -1,6 +1,6 @@
 <?php
-$db_servidor = 'localhost';
-$db_usuario = 'root';
-$db_password = '';
+$db_servidor = '65.99.225.119';
+$db_usuario = 'caitecmx_covitec';
+$db_password = 'HZ9bKz2PSlNY';
 $db_basededatos = 'caitecmx_covitec';
 ?>
