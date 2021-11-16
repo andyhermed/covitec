8fe698b416a1e47e88c7560fd73af24de0a9b9b1
AnddyHub <andreabt40@gmail.com>
2021-11-15 21:03:45 -0600

    m

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de Control de acceso</title>
+    <title>Sistema de Control de Acceso</title>
     <link rel="stylesheet" href="css/styles_log.css">
 </head>
 <body>
